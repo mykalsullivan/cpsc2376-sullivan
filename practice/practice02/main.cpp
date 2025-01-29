0d88bbb41b5190afa7e69bcc0c014db28bbfedc5
Mykal Sullivan <80817911+mykalsullivan@users.noreply.github.com>
2025-01-29 13:53:08 -0600

    Added practice02: "PersistentBankAccount". It has not been programmed yet. Do that, nerd.

---
+++ practice/practice02/main.cpp
@@ -0,0 +1,30 @@
+//
+// Created by msullivan on 1/29/25.
+//
+
+/* Instructions:
+    * Store the balance in a file named account_balance.txt.
+    * On program startup:
+    * If account_balance.txt does not exist, create it and set the initial balance to $100.00.
+    * If the file exists, read the current balance from the file.
+    * Allow the user to perform the following actions:
+        - Check the balance.
+        - Deposit money (must be a positive amount).
+        - Withdraw money (cannot exceed the current balance or be negative).
+        - Update account_balance.txt immediately after every transaction.
+
+    Requirements:
+w
+    Ensure file operations are handled securely and gracefully. For example:
+    * If the file cannot be opened, display an appropriate error message and exit.
+    * Validate all user inputs
+        - Deposits must be positive values.
+        - Withdrawals must not exceed the current balance and must be positive values.
+    * The program should be interactive, displaying a menu for the user to choose operations.
+ */
+
+int main()
+{
+
+    return 0;
+}
