c236d2a23967757a39e17bd79f8a453bb9d9a960
Mykal Sullivan <80817911+mykalsullivan@users.noreply.github.com>
2025-03-06 23:18:37 -0600

    Finished problem3a for practice05. It is now ready to be submitted.

--- practice/practice05/problem3a.cpp
+++ practice/practice05/problem3a.cpp
@@ -2,18 +2,16 @@
 // Created by msullivan on 2/26/25.
 //
 
+#include <algorithm>
+
 #include "Input.h"
 #include <iostream>
+#include <numeric>
 #include <list>
-
-struct Task {
-    std::string title;
-    std::string description;
-};
 
 class Application {
     bool m_Running {false};
-    std::list<Task> m_Tasks;
+    std::list<int> m_Ints {};
 
 public:
     Application() = default;
@@ -21,85 +19,45 @@ public:
 
     int exec()
     {
-        std::cout << "Simple Task Manager by M. Sullivan\n";
+        std::cout << "Algorithms (std::accumulate) by M. Sullivan\n";
 
-        m_Running = true;
-        while (m_Running)
-        {
-            std::string prompt = "1: Add task\n2: Remove task\n3: View tasks\n4: Exit\nWhich one?";
-            handleAction(Input::numberPrompt(prompt, 1, 4, false));
-        }
+        gatherIntegers();
+        printEachElementSquared();
+        sumAllSquaredValuesAndPrintResult();
+
         return 0;
     }
 
 private:
-    void handleAction(const int action)
+    void gatherIntegers()
     {
-        switch (action)
+        const std::string prompt = "Enter a number (0 when finished)";
+        int input {};
+
+        do
         {
-            case 1: addTask(); break;
-            case 2: removeTask(); break;
-            case 3: showTasks(true); break;
-            case 4: stop(); break;
-            default:
-                std::cout << "Invalid selection.\n"; break;
-        }
+            input = Input::numberPrompt(prompt, std::numeric_limits<int>::min(), std::numeric_limits<int>::max(), false);
+            m_Ints.emplace_back(input);
+        } while (input != 0);
     }
 
-    void addTask()
+    void printEachElementSquared()
     {
-        Task newTask;
-        newTask.title = Input::textPrompt("Title", 0, 64, true);
-        newTask.description = Input::textPrompt("Description", 0, 500, true);
-        m_Tasks.emplace_back(newTask);
+        int i {0};
+        std::for_each(m_Ints.begin(), m_Ints.end(), [&i](int &value)
+        {
+            value *= value;
+            std::cout << '#' << ++i << " squared: " << value << '\n';
+        });
     }
 
-    void removeTask()
+    void sumAllSquaredValuesAndPrintResult()
     {
-        // Return if the task list is empty
-        if (m_Tasks.empty())
+        const int squaredValuesSum = std::accumulate(m_Ints.begin(), m_Ints.end(), 0, [](const int total, const int num)
         {
-            std::cout << "No tasks.\n";
-            return;
-        }
-
-        // Display short list of tasks on the list
-        showTasks(false);
-
-        const std::string prompt {"Select which one to remove (0 to cancel)"};
-        const int selection = Input::numberPrompt(prompt, 0, static_cast<int>(m_Tasks.size()), true);
-
-        // Entering 0 cancels the operation
-        if (selection == 0) return;
-
-        if (selection > static_cast<int>(m_Tasks.size()))
-        {
-            std::cout << "Invalid selection.\n";
-            return;
-        }
-
-        // Remove an item
-        const auto it = std::next(m_Tasks.begin(), selection - 1);
-        m_Tasks.erase(it);
-        std::cout << "Removed task " << selection << ".\n";
-    }
-
-    void showTasks(const bool withDescription)
-    {
-        // Return if the task list is empty
-        if (m_Tasks.empty())
-        {
-            std::cout << "No tasks.\n";
-            return;
-        }
-
-        int i = 1;
-        for (const auto &[title, description] : m_Tasks)
-        {
-            std::cout << i << ": \"" << title << "\"\n";
-            if (withDescription) std::cout << "\t- " << description << '\n';
-            ++i;
-        }
+            return total + num;
+        });
+        std::cout << "Sum of squared list elements: " << squaredValuesSum << '\n';
     }
 
     void stop()
